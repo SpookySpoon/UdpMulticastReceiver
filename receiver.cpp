7164b0a80ca729870a5dcf627da65869bb5a5b5e
SpookySpoon <xoxlovval@gmail.com>
2016-12-06 04:02:27 +0300

    ''

--- receiver.cpp
+++ receiver.cpp
@@ -2,14 +2,13 @@
 #include <QDebug>
 #include "pbuff.h"
 #include "receiver.h"
-#include "packageFormat.pb.h"
 
 Receiver::Receiver(int someFilePort, int someResPort, const QHostAddress& someGroupAddress, const QString& someSavePath, QObject *parent)
     : QUdpSocket(parent),filePort(someFilePort), responsePort(someResPort),groupAddress(someGroupAddress), dFolder(someSavePath)
 {
     initRec();
     connect(this, SIGNAL(readyRead()),this, SLOT(processPendingDatagrams()));
-    connect(this, SIGNAL(gotPackage(QString)),this, SLOT(reportPackage(QString)));
+    connect(this, SIGNAL(gotPackage(UdpStream::UdpBytes::PackStatus)),this, SLOT(reportPackage(UdpStream::UdpBytes::PackStatus)));
 }
 
 void Receiver::initRec()
@@ -21,39 +20,44 @@ void Receiver::initRec()
 
 void Receiver::processPendingDatagrams()
 {
-    qDebug()<<"receiving data";
     QByteArray fileBytes;
-    fileBytes.resize(this->pendingDatagramSize());
-    this->readDatagram(fileBytes.data(), fileBytes.size());
-    udpStream::updBytes tempPackage=ProtoBytes<udpStream::updBytes>::protoFromByteArray(fileBytes);
-    if(tempPackage.packid()==0)
+    while (this->hasPendingDatagrams())
     {
+        fileBytes.resize(this->pendingDatagramSize());
+        this->readDatagram(fileBytes.data(), fileBytes.size());
+    }
+    UdpStream::UdpBytes tempPackage=ProtoBytes<UdpStream::UdpBytes>::protoFromByteArray(fileBytes);
+    if(tempPackage.packstatus()==UdpStream::UdpBytes::PackStatus::UdpBytes_PackStatus_FIRST)
+    {
+        totalBytes="";
         filePath=QString("%1\\%2").arg(dFolder)
-                .arg(QString::fromStdString(tempPackage.pack()));
+                .arg(QString::fromStdString(tempPackage.packcontent()));
         recentDatagrammID=0;
-        emit gotPackage("We got the stuff!");
+        emit gotPackage(tempPackage.packstatus());
     }
-    if(tempPackage.pack()=="{konetsN@hy%}")
-    {
-        reportPackage("stop");
-        QFile myFile(filePath);
-        myFile.open(QIODevice::ReadWrite);
-        myFile.write(totalBytes);
-        myFile.close();
-        qDebug()<<"Transfer finished";
-    }
-    if(recentDatagrammID!=tempPackage.packid())
+
+    if(recentDatagrammID-tempPackage.packid()==-1)
     {
         recentDatagrammID=tempPackage.packid();
-        totalBytes.append(tempPackage.pack().data());
-        emit gotPackage("We got the stuff!");
-        qDebug()<<"bytes received: "<<fileBytes.count();
+        totalBytes.append(tempPackage.packcontent().data());
+        emit gotPackage(tempPackage.packstatus());
+        qDebug()<<"bytes received: "<<fileBytes.size();
+        if(tempPackage.packstatus()==UdpStream::UdpBytes::PackStatus::UdpBytes_PackStatus_LAST)
+        {
+            QFile myFile(filePath);
+            myFile.open(QIODevice::ReadWrite);
+            myFile.write(totalBytes);
+            myFile.close();
+            qDebug()<<"Transfer finished. Filename: "<<filePath;
+        }
     }
 }
 
-void Receiver::reportPackage(const QString& mes)
+void Receiver::reportPackage(UdpStream::UdpBytes::PackStatus mes)
 {
-    QByteArray opa = mes.toLatin1();
+    UdpStream::UdpBytes responce;
+    responce.set_packstatus(mes);
+    QByteArray opa = ProtoBytes<UdpStream::UdpBytes>::protoToByteArray(responce);
     int rep=this->writeDatagram(opa.data(), opa.size(),
                                  groupAddress, responsePort);
     qDebug()<<"reported bytes: "<<rep;
--- receiver.h
+++ receiver.h
@@ -1,18 +1,17 @@
 #pragma once
 #include <QUdpSocket>
 #include <QHostAddress>
-
+#include "packageFormat.pb.h"
 
 class Receiver : public QUdpSocket
 {
     Q_OBJECT
-
 public:
     Receiver(int,int,const QHostAddress&, const QString&, QObject *parent = 0);
 private slots:
     void processPendingDatagrams();
     void initRec();
-    void reportPackage(const QString&);
+    void reportPackage(UdpStream::UdpBytes::PackStatus);
 private:
     QString dFolder, filePath;
     QHostAddress groupAddress;
@@ -20,5 +19,5 @@ private:
     int recentDatagrammID=-1;
     QByteArray totalBytes;
 signals:
-    void gotPackage(const QString&);
+    void gotPackage(UdpStream::UdpBytes::PackStatus);
 };
